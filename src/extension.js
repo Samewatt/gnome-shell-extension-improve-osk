9b2eba5a8900c9f715cff363a037d28dde351862
Samewatt <salexwattson@gmail.com>
2020-07-11 18:45:03 -0700

    changed layout and fixed icons

--- src/extension.js
+++ src/extension.js
@@ -1,3 +1,7 @@
+//https://gitlab.gnome.org/GNOME/gnome-shell/-/blob/master/js/ui/keyboard.js    gnome osk
+//https://github.com/schuhumi/gnome-shell-extension-improve-osk                 original
+//https://github.com/dmroduner/gnome-shell-extension-improve-osk                copied fork
+//https://gitlab.gnome.org/GNOME/gnome-shell/-/tree/master/data/osk-layouts     default layouts
 
 const St = imports.gi.St;
 const Main = imports.ui.main;
@@ -11,15 +15,134 @@ const Tweener = imports.ui.tweener;
 const Overview = imports.ui.overview;
 const Layout = imports.ui.layout;
 
-var defaultKeyboardDelay;
 var Backup_DefaultKeysForRow;
 var Backup_contructor;
 var Backup_keyvalPress;
 var Backup_keyvalRelease;
 
+const defaultKeysPreMod = [
+    [ 
+        [{ label: 'Esc', width: 1, keyval: Clutter.KEY_Escape }], 
+        [{ label: '↹', width: 1.5, keyval: Clutter.KEY_Tab }], 
+        [{ width: 2.5, level: 1, extraClassName: 'shift-key-lowercase', icon: 'keyboard-shift-filled-symbolic' }], 
+        [
+            { label: 'Ctrl', width: 1, keyval: Clutter.KEY_Control_L, extraClassName: 'control-key' }, 
+            { label: 'Alt', width: 1, keyval: Clutter.KEY_Alt_L, extraClassName: 'alt-key' }
+        ] 
+    ],
+    [ 
+        [{ label: 'Esc', width: 1, keyval: Clutter.KEY_Escape }], 
+        [{ label: '↹', width: 1.5, keyval: Clutter.KEY_Tab }], 
+        [{ width: 2.5, level: 0, extraClassName: 'shift-key-uppercase', icon: 'keyboard-shift-filled-symbolic'  }], 
+        [
+            { label: 'Ctrl', width: 1, keyval: Clutter.KEY_Control_L, extraClassName: 'control-key' }, 
+            { label: 'Alt', width: 1, keyval: Clutter.KEY_Alt_L, extraClassName: 'alt-key' }
+        ] 
+    ],
+    [ 
+        [{ label: 'Esc', width: 1, keyval: Clutter.KEY_Escape }], 
+        [{ label: '↹', width: 1.5, keyval: Clutter.KEY_Tab }], 
+        [{ label: '=/<', width: 2.5, level: 3}], 
+        [
+            { label: 'Ctrl', width: 1, keyval: Clutter.KEY_Control_L, extraClassName: 'control-key' }, 
+            { label: 'Alt', width: 1, keyval: Clutter.KEY_Alt_L, extraClassName: 'alt-key' }
+        ] 
+    ],
+    [ 
+        [{ label: 'Esc', width: 1, keyval: Clutter.KEY_Escape }], 
+        [{ label: '↹', width: 1, keyval: Clutter.KEY_Tab }], 
+        [{ label: '?123', width: 1.5, level: 2 }], 
+        [
+            { label: 'Ctrl', width: 1, keyval: Clutter.KEY_Control_L, extraClassName: 'control-key' }, 
+            { label: 'Alt', width: 1, keyval: Clutter.KEY_Alt_L, extraClassName: 'alt-key' }
+        ] 
+    ]
+];
+
+    
+const defaultKeysPostMod = [
+    [ 
+        [
+            { width: 1.5, keyval: Clutter.KEY_BackSpace, icon: 'edit-clear-symbolic' }, 
+            { action: 'emoji', icon: 'face-smile-symbolic' }
+        ], [                
+            { width: 2, keyval: Clutter.KEY_Return, extraClassName: 'enter-key', icon: 'keyboard-enter-symbolic' }, 
+            { width: 1, action: 'languageMenu', extraClassName: 'layout-key', icon: 'keyboard-layout-filled-symbolic' }
+        ], [
+            { label: '?123', width: 1, level: 2 },
+            { label: '↑', width: 1, keyval: Clutter.KEY_Up }, 
+            { width: 2, level: 1, right: true, extraClassName: 'shift-key-lowercase', icon: 'keyboard-shift-filled-symbolic' }
+        ], [
+            { label: '←', width: 1, keyval: Clutter.KEY_Left }, 
+            { label: '↓', width: 1, keyval: Clutter.KEY_Down }, 
+            { label: '→', width: 1, keyval: Clutter.KEY_Right }, 
+            { width: 1, action: 'hide', extraClassName: 'hide-key', icon: 'go-down-symbolic' }
+        ]
+    ], [
+        [
+            { width: 1.5, keyval: Clutter.KEY_BackSpace, icon: 'edit-clear-symbolic' }, 
+            { action: 'emoji', icon: 'face-smile-symbolic' }
+        ], [
+            { width: 2, keyval: Clutter.KEY_Return, extraClassName: 'enter-key' }, 
+            { width: 1, action: 'languageMenu', extraClassName: 'layout-key', icon: 'keyboard-layout-filled-symbolic' }
+        ], [
+            { label: '?123', width: 1, level: 2 },
+            { label: '↑', width: 1, keyval: Clutter.KEY_Up }, 
+            { width: 2, level: 0, right: true, extraClassName: 'shift-key-uppercase', icon: 'keyboard-shift-filled-symbolic' }
+        ], [
+            { label: '←', width: 1, keyval: Clutter.KEY_Left }, 
+            { label: '↓', width: 1, keyval: Clutter.KEY_Down }, 
+            { label: '→', width: 1, keyval: Clutter.KEY_Right }, 
+            { width: 1, action: 'hide', extraClassName: 'hide-key', icon: 'go-down-symbolic' }
+        ] 
+    ], [ 
+        [
+            { width: 1.5, keyval: Clutter.KEY_BackSpace, icon: 'edit-clear-symbolic' }, 
+            { action: 'emoji', icon: 'face-smile-symbolic' }
+        ], [
+            { width: 2, keyval: Clutter.KEY_Return, extraClassName: 'enter-key', icon: 'keyboard-enter-symbolic' }, 
+            { width: 1, action: 'languageMenu', extraClassName: 'layout-key', icon: 'keyboard-layout-filled-symbolic' }
+        ], [
+            { label: 'ABC', width: 1, level: 0 },
+            { label: '↑', width: 1, keyval: Clutter.KEY_Up }, 
+            { label: '=/<', width: 2, level: 3, right: true } 
+        ], [
+            { label: '←', width: 1, keyval: Clutter.KEY_Left }, 
+            { label: '↓', width: 1, keyval: Clutter.KEY_Down }, 
+            { label: '→', width: 1, keyval: Clutter.KEY_Right }, 
+            { width: 1, action: 'hide', extraClassName: 'hide-key', icon: 'go-down-symbolic' }
+        ] 
+    ], [   
+        [
+            { label: 'F1', width: 1, keyval: Clutter.KEY_F1 }, 
+            { label: 'F2', width: 1, keyval: Clutter.KEY_F2 }, 
+            { label: 'F3', width: 1, keyval: Clutter.KEY_F3 }, 
+            { width: 1.5, keyval: Clutter.KEY_BackSpace, icon: 'edit-clear-symbolic' }, 
+            { width: 1, action: 'hide', extraClassName: 'hide-key', icon: 'go-down-symbolic' }
+        ], [
+            { label: 'F4', width: 1, keyval: Clutter.KEY_F4 }, 
+            { label: 'F5', width: 1, keyval: Clutter.KEY_F5 }, 
+            { label: 'F6', width: 1, keyval: Clutter.KEY_F6 }, 
+            { width: 2, keyval: Clutter.KEY_Return, extraClassName: 'enter-key', icon: 'keyboard-enter-symbolic' }, 
+            { width: 1.5, action: 'languageMenu', extraClassName: 'layout-key', icon: 'keyboard-layout-filled-symbolic' }
+        ], [
+            { label: 'F7', width: 1, keyval: Clutter.KEY_F7 }, 
+            { label: 'F8', width: 1, keyval: Clutter.KEY_F8 }, 
+            { label: 'F9', width: 1, keyval: Clutter.KEY_F9 }, 
+            { label: '?123', width: 3, level: 2, right: true }, 
+            { label: 'ABC', width: 1.5, level: 0 }
+        ], [
+            { label: 'F10', width: 1, keyval: Clutter.KEY_F10 }, 
+            { label: 'F11', width: 1, keyval: Clutter.KEY_F11 }, 
+            { label: 'F12', width: 1, keyval: Clutter.KEY_F12 }, 
+            { label: '←', width: 1, keyval: Clutter.KEY_Left }, 
+            { label: '↑', width: 1, keyval: Clutter.KEY_Up }, 
+            { label: '↓', width: 1, keyval: Clutter.KEY_Down }, 
+            { label: '→', width: 1, keyval: Clutter.KEY_Right }] 
+    ],
+];
 
 function init() {
-    defaultKeyboardDelay = Layout.KEYBOARD_ANIMATION_TIME;
     Backup_DefaultKeysForRow = Keyboard.Keyboard.prototype['_getDefaultKeysForRow'];
     Backup_contructor = Keyboard.KeyboardController.prototype['constructor'];
     Backup_keyvalPress = Keyboard.KeyboardController.prototype['keyvalPress'];
@@ -43,39 +166,8 @@ function enable() {
         }
     }
     
-
     Keyboard.Keyboard.prototype['_getDefaultKeysForRow'] = function(row, numRows, level) {
         
-        let defaultKeysPreMod = [
-            [ [{ label: 'Esc', width: 1, keyval: Clutter.KEY_Escape }], [{ label: '↹', width: 1.5, keyval: Clutter.KEY_Tab }], [{ width: 1.5, level: 1, extraClassName: 'shift-key-lowercase' }], [{ label: 'Ctrl', width: 1, keyval: Clutter.KEY_Control_L, extraClassName: 'control-key' }, { label: '◆', width: 1, keyval: Clutter.KEY_Super_L, extraClassName: 'super-key' }, { label: 'Alt', width: 1, keyval: Clutter.KEY_Alt_L, extraClassName: 'alt-key' }] ],
-            [ [{ label: 'Esc', width: 1, keyval: Clutter.KEY_Escape }], [{ label: '↹', width: 1.5, keyval: Clutter.KEY_Tab }], [{ width: 1.5, level: 0, extraClassName: 'shift-key-uppercase' }], [{ label: 'Ctrl', width: 1, keyval: Clutter.KEY_Control_L, extraClassName: 'control-key' }, { label: '◆', width: 1, keyval: Clutter.KEY_Super_L, extraClassName: 'super-key' }, { label: 'Alt', width: 1, keyval: Clutter.KEY_Alt_L, extraClassName: 'alt-key' }] ],
-            [ [{ label: 'Esc', width: 1, keyval: Clutter.KEY_Escape }], [{ label: '↹', width: 1.5, keyval: Clutter.KEY_Tab }], [{ label: '=/<F', width: 1.5, level: 3}], [{ label: 'Ctrl', width: 1, keyval: Clutter.KEY_Control_L, extraClassName: 'control-key' }, { label: '◆', width: 1, keyval: Clutter.KEY_Super_L, extraClassName: 'super-key' }, { label: 'Alt', width: 1, keyval: Clutter.KEY_Alt_L, extraClassName: 'alt-key' }] ],
-            [ [{ label: 'Esc', width: 1, keyval: Clutter.KEY_Escape }], [{ label: '↹', width: 1.5, keyval: Clutter.KEY_Tab }], [{ label: '?123', width: 1.5, level: 2 }], [{ label: 'Ctrl', width: 1, keyval: Clutter.KEY_Control_L, extraClassName: 'control-key' }, { label: '◆', width: 1, keyval: Clutter.KEY_Super_L, extraClassName: 'super-key' }, { label: 'Alt', width: 1, keyval: Clutter.KEY_Alt_L, extraClassName: 'alt-key' }] ],
-        ];
-
-    
-        let defaultKeysPostMod = [
-            [ [{ label: '⌫', width: 1.5, keyval: Clutter.KEY_BackSpace }, { label: '⌦', width: 1, keyval: Clutter.KEY_Delete }, { width: 1, action: 'hide', extraClassName: 'hide-key' }],
-              [{ width: 2, keyval: Clutter.KEY_Return, extraClassName: 'enter-key' }, { width: 1.5, action: 'languageMenu', extraClassName: 'layout-key' }],
-              [{ width: 3, level: 1, right: true, extraClassName: 'shift-key-lowercase' }, { label: '?123', width: 1.5, level: 2 }],
-              [{ label: '←', width: 1, keyval: Clutter.KEY_Left }, { label: '↑', width: 1, keyval: Clutter.KEY_Up }, { label: '↓', width: 1, keyval: Clutter.KEY_Down }, { label: '→', width: 1, keyval: Clutter.KEY_Right }] ],
-            [ [{ label: '⌫', width: 1.5, keyval: Clutter.KEY_BackSpace }, { label: '⌦', width: 1, keyval: Clutter.KEY_Delete }, { width: 1, action: 'hide', extraClassName: 'hide-key' }],
-              [{ width: 2, keyval: Clutter.KEY_Return, extraClassName: 'enter-key' }, { width: 1.5, action: 'languageMenu', extraClassName: 'layout-key' }],
-              [{ width: 3, level: 0, right: true, extraClassName: 'shift-key-uppercase' }, { label: '?123', width: 1.5, level: 2 }],
-              [{ label: '←', width: 1, keyval: Clutter.KEY_Left }, { label: '↑', width: 1, keyval: Clutter.KEY_Up }, { label: '↓', width: 1, keyval: Clutter.KEY_Down }, { label: '→', width: 1, keyval: Clutter.KEY_Right }] ],
-            [ [{ label: '⌫', width: 1.5, keyval: Clutter.KEY_BackSpace }, { label: '⌦', width: 1, keyval: Clutter.KEY_Delete }, { width: 1, action: 'hide', extraClassName: 'hide-key' }],
-              [{ width: 2, keyval: Clutter.KEY_Return, extraClassName: 'enter-key' }, { width: 1.5, action: 'languageMenu', extraClassName: 'layout-key' }],
-              [{ label: '=/<F', width: 3, level: 3, right: true }, { label: 'ABC', width: 1.5, level: 0 }],
-              [{ label: '←', width: 1, keyval: Clutter.KEY_Left }, { label: '↑', width: 1, keyval: Clutter.KEY_Up }, { label: '↓', width: 1, keyval: Clutter.KEY_Down }, { label: '→', width: 1, keyval: Clutter.KEY_Right }] ],
-            [ [{ label: 'F1', width: 1, keyval: Clutter.KEY_F1 }, { label: 'F2', width: 1, keyval: Clutter.KEY_F2 }, { label: 'F3', width: 1, keyval: Clutter.KEY_F3 }, { label: '⌫', width: 1.5, keyval: Clutter.KEY_BackSpace }, { label: '⌦', width: 1, keyval: Clutter.KEY_Delete }, { width: 1, action: 'hide', extraClassName: 'hide-key' }],
-              [{ label: 'F4', width: 1, keyval: Clutter.KEY_F4 }, { label: 'F5', width: 1, keyval: Clutter.KEY_F5 }, { label: 'F6', width: 1, keyval: Clutter.KEY_F6 }, { width: 2, keyval: Clutter.KEY_Return, extraClassName: 'enter-key' }, { width: 1.5, action: 'languageMenu', extraClassName: 'layout-key' }],
-              [{ label: 'F7', width: 1, keyval: Clutter.KEY_F7 }, { label: 'F8', width: 1, keyval: Clutter.KEY_F8 }, { label: 'F9', width: 1, keyval: Clutter.KEY_F9 }, { label: '?123', width: 3, level: 2, right: true }, { label: 'ABC', width: 1.5, level: 0 }],
-              [{ label: 'F10', width: 1, keyval: Clutter.KEY_F10 }, { label: 'F11', width: 1, keyval: Clutter.KEY_F11 }, { label: 'F12', width: 1, keyval: Clutter.KEY_F12 }, { label: '←', width: 1, keyval: Clutter.KEY_Left }, { label: '↑', width: 1, keyval: Clutter.KEY_Up }, { label: '↓', width: 1, keyval: Clutter.KEY_Down }, { label: '→', width: 1, keyval: Clutter.KEY_Right }] ],
-        ];
-
-    
-        let pre, post;
-
         /* The first 2 rows in defaultKeysPre/Post belong together with
          * the first 2 rows on each keymap. On keymaps that have more than
          * 4 rows, the last 2 default key rows must be respectively
@@ -103,7 +195,6 @@ function enable() {
         this._currentSource = this._inputSourceManager.currentSource;
         
         this._controlActive = false;
-        this._superActive = false;
         this._altActive = false;
 
         Main.inputMethod.connect('notify::content-purpose',
@@ -120,9 +211,6 @@ function enable() {
         if(keyval==Clutter.KEY_Control_L) {
             this._controlActive = !this._controlActive; // This allows to revert an accidental tap on Ctrl by tapping on it again
         }
-        if(keyval==Clutter.KEY_Super_L) {
-            this._superActive = !this._superActive;
-        }
         if(keyval==Clutter.KEY_Alt_L) {
             this._altActive = !this._altActive;
         }
@@ -136,16 +224,6 @@ function enable() {
             this._virtualDevice.notify_keyval(Clutter.get_current_event_time(),
                                               Clutter.KEY_Control_L, Clutter.KeyState.RELEASED);
             Main.layoutManager.keyboardBox.remove_style_class_name("control-key-latched");
-        }
-        if(this._superActive)
-        {
-            this._virtualDevice.notify_keyval(Clutter.get_current_event_time(),
-                                              Clutter.KEY_Super_L, Clutter.KeyState.PRESSED);
-            Main.layoutManager.keyboardBox.add_style_class_name("super-key-latched");
-        } else {
-            this._virtualDevice.notify_keyval(Clutter.get_current_event_time(),
-                                              Clutter.KEY_Super_L, Clutter.KeyState.RELEASED);
-            Main.layoutManager.keyboardBox.remove_style_class_name("super-key-latched");
         }
         if(this._altActive)
         {
@@ -163,7 +241,7 @@ function enable() {
 
     
     Keyboard.KeyboardController.prototype['keyvalRelease'] = function(keyval) {
-        if(keyval==Clutter.KEY_Control_L || keyval==Clutter.KEY_Alt_L || keyval==Clutter.KEY_Super_L) {
+        if(keyval==Clutter.KEY_Control_L || keyval==Clutter.KEY_Alt_L ) {
             return;
         }
         
@@ -177,13 +255,6 @@ function enable() {
             this._controlActive = false;
             Main.layoutManager.keyboardBox.remove_style_class_name("control-key-latched");
         }
-        if(this._superActive)
-        {
-            this._virtualDevice.notify_keyval(Clutter.get_current_event_time(),
-                                              Clutter.KEY_Super_L, Clutter.KeyState.RELEASED);
-            this._superActive = false;
-            Main.layoutManager.keyboardBox.remove_style_class_name("super-key-latched");
-        }
         if(this._altActive)
         {
             this._virtualDevice.notify_keyval(Clutter.get_current_event_time(),
@@ -194,11 +265,10 @@ function enable() {
 
     }
 
-    Layout.KEYBOARD_ANIMATION_TIME = 0;
     if(KeyboardIsSetup) {
         Main.keyboard._setupKeyboard();
     }
-    Main.layoutManager.addChrome(   Main.layoutManager.keyboardBox, { affectsStruts: true, trackFullscreen: false });
+    Main.layoutManager.addChrome(Main.layoutManager.keyboardBox);
 }
 
 function disable() {
@@ -222,9 +292,8 @@ function disable() {
     Keyboard.KeyboardController.prototype['constructor'] = Backup_contructor;
     Keyboard.KeyboardController.prototype['keyvalPress'] = Backup_keyvalPress;
     Keyboard.KeyboardController.prototype['keyvalRelease'] = Backup_keyvalRelease;
-    Layout.KEYBOARD_ANIMATION_TIME = defaultKeyboardDelay;
     if(KeyboardIsSetup) {
         Main.keyboard._setupKeyboard();
     }
-    Main.layoutManager.addChrome(   Main.layoutManager.keyboardBox);
+    Main.layoutManager.addChrome(Main.layoutManager.keyboardBox);
 }
